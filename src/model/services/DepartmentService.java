83d4b98d1654bae6a772460cb3a76b5324b112a6
Igor Santos <igorsantos-silva123@outlook.com>
2020-12-15 16:10:10 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -12,5 +12,15 @@ public class DepartmentService {
 	
 	public List<Department> findAll(){
 		return dao.findAll();
-}
+	}
+	
+	public void saveOrUpdate(Department obj) {
+		//se o id for igual a null, indica que é um novo departamento para  ser inserido no banco
+		//caso já exista será atualizado o departamento com o mesmo id
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}else {
+			dao.update(obj);
+		}
+	}
 }
